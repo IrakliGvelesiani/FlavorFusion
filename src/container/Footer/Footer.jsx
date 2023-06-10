1619205a78f2c9df395aad42f81038392f173421
IrakliGvelesiani <104423165+IrakliGvelesiani@users.noreply.github.com>
2023-06-10 04:22:30 +0400

    FlavorFusion

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">123 - Tbilisi, Georgia</p>
+        <p className="p__opensans">+995 555 555 555</p>
+        <p className="p__opensans">+995 555 555 555</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -46,7 +46,11 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">
+        <a href="https://github.com/IrakliGvelesiani" target="_blank">
+          © IrakliGvelesiani. All rigths reserved
+        </a>
+      </p>
     </div>
   </div>
 );
